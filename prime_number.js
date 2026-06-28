5831885ff4d510a48209ede7d7742fcd2bf0bf76
aalok2004 <raghuvanshiaalok@gmail.com>
2026-06-28 09:57:51 +0530

    prime number

---
+++ prime_number.js
@@ -0,0 +1,31 @@
+// var n=1;
+// var count =0;
+// for(var i=2;i<n;i++){
+//      if(n%i==0){
+//         count++;
+//      }
+
+// }
+// if(count==0){
+//     console.log("prime")
+// }else{
+//      console.log("not prime")
+// }
+
+
+var n=100;
+
+for(var j=2;j<=n;j++){
+var count =0;
+
+for(var i=2;i<j;i++){
+     if(j%i==0){
+        count++;
+     }
+
+}
+if(count==0){
+    console.log(j)
+}
+
+}
