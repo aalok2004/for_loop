19ae12288b2ccf375cd239254a2ddc58cebfae19
aalok2004 <raghuvanshiaalok@gmail.com>
2026-06-30 09:06:46 +0530

    find strong number

---
+++ strong_number.js
@@ -0,0 +1,38 @@
+// //17. strong 
+// var n = 200;
+// var sum = 0;
+// for(var i=n;i>0;i=parseInt(i/10)){
+// var digit =i%10;
+// var fact= 1;      // or isme me ye variable ko upper hi initilaize kar raha tha
+// for(var j=1;j<=digit;j++){
+// fact=fact*j;
+// }
+// sum=sum+fact;
+// }
+// //console.log(sum)
+// if(n==sum){
+// console.log(n);
+// }else{
+//     console.log("this is not a strong number ")
+// }
+
+//17. strong 
+var n = 145;
+var sum = 0;
+for(var i=n;i>0;i=parseInt(i/10)){
+var digit =i%10;
+var fact= 1;      // or isme me ye variable ko upper hi initilaize kar raha tha
+for(var j=1;j<=digit;j++){
+fact=fact*j;
+console.log(fact);
+}
+sum=sum+fact;
+}
+//console.log(sum)
+if(n==sum){
+console.log(n);
+}else{
+    console.log("this is not a strong number ")
+}
+
+
